2ac0ac9f5c4dda67e7f1b7891d12871baba646cc
anoohya29 <103990893+anoohya29@users.noreply.github.com>
2023-09-20 15:09:41 -0400

    Update hw02_06.lean

--- HW_01/hw02_06.lean
+++ HW_01/hw02_06.lean
@@ -1,21 +1,21 @@
 import Mathlib.Data.Real.Basic
 import Library.Tactic.Extra
 import Library.Tactic.Numbers
-
+---6(a)
 example {x y : ℤ} (h1 : x + 3 ≥ 2 * y) (h2 : 1 ≤ y) : x ≥ -1 :=
   calc
    x = x + 3 -3 := by ring 
    _ ≥ (2 * y ) -3 := by rel[h1]
    _ ≥ (2 * 1) -3 := by rel[h2]
    _ ≥ -1 := by numbers 
-
+---6(b)
 example {a b : ℚ} (h1 : 3 ≤ a) (h2 : a + 2 * b ≥ 4) : a + b ≥ 3 :=
   calc
     a + b = (a + 2*b + a)/2 := by ring 
     _ ≥ (4 + 3)/2 := by rel[h1,h2]
     _ ≥ 3 := by numbers 
 
-
+---6(c)
 example {x : ℤ} (hx : x ≥ 9) : x ^ 3 - 8 * x ^ 2 + 2 * x ≥ 3 :=
   calc 
     x ^ 3 - 8 * x ^ 2 + 2 * x = x*(x*x - 8*x +2) := by ring 
