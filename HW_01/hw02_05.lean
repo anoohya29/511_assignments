1098d8c836c32a7b87ca95845267c2d4ccfe302d
anoohya29 <103990893+anoohya29@users.noreply.github.com>
2023-09-20 23:25:11 -0400

    Update hw02_05.lean

--- HW_01/hw02_05.lean
+++ HW_01/hw02_05.lean
@@ -1,28 +1,38 @@
 --(a)
-  lemma de_morgan_2 {p q : Prop} : (¬p ∧ ¬q) → ¬(p ∨ q) :=  by 
+lemma de_morgan_2 {p q : Prop} : (¬p ∧ ¬q) → ¬(p ∨ q) := by
+-- by closing box [1-11]
   intro h
   have h_not_p := h.left,
   have h_not_q := h.right,
   have h1: p → False := by
-  { intro h_p,
-    exact h_not_p h_p },
-    have h2: q → False := by
-    { intro h_q,
-  
-    exact h_not_q h_q },
+-- by closing box [5-6]
+  { intro hp,
+    exact h_not_p hp },
+  have h2: q → False := by
+--  by closing box [7-8]
+  { intro hq,
+    exact h_not_q hq },
   intro h_or,
   apply Or.elim h_or h1 h2
---(b)
-  lemma de_morgan_3 {p q : Prop} : (¬p ∨ ¬q) → ¬(p ∧ q) := by
 
-  intro h_or
-  have h1: ¬p → False := by
-    { intro h_not_p,  --[5,6]
-      intro h_and,
-      exact h_not_p h_and.left },
-  have h2: ¬q → False := by
-  { intro h_not_q,    --[7-8]
-    intro h_and,
-    exact h_not_q h_and.right },
+
+--(b)
+lemma de_morgan_3 {p q : Prop} : (¬p ∨ ¬q) → ¬(p ∧ q) := by 
+-- by closing box [1-10]
+  intro h,
   intro h_and,
-  apply Or.elim h_or h1 h2
+  have hp := h_and.left,
+  have hq := h_and.right,
+-- by closing box [2-9]
+  apply Or.elim h,
+-- by closing box [5-6]
+  { intro h_not_p,
+    show False,
+    apply h_not_p,
+    exact hp },
+--  by closing box [7-8]
+  { intro h_not_q,
+    show False,
+    apply h_not_q,
+    exact hq }
+
